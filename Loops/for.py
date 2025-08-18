b46e5ab5ece624332ac89cf41f8d3375abc8a63f
Manas Ranjan Bera <manasbera484@gmail.com>
2025-08-18 16:08:28 +0530

    Loops

---
+++ Loops/for.py
@@ -0,0 +1,51 @@
+# for condition:
+#     # instructions
+
+marks = [90, 10, 60, 20, 30, 20, 40, 50]
+# for mark in marks:
+#     print(mark)
+
+# for mark in marks:
+#     # print(mark+20)
+#     if mark >= 30:
+#         print(mark,"is pass")
+#     else:
+#         print(mark,"is fail")
+
+# stoping_point = 30
+# for mark in marks:
+#     print(mark)
+#     if mark == stoping_point:
+#         break
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(1,10):
+#     print(i)
+
+# Nested for loop
+# a =1
+# for i in range(1,5):
+#     for j in range(1,5):
+#         for k in range(1,5):
+#            for l in range(1,5):
+#                 for m in range(1,5):
+#                     print(i,j,k,l,m)
+#                     print(a)
+#                     a += 1
+
+# While loop
+# while condition:
+#     # instructions
+
+# i = 1
+# while i < 10:
+#     print(i)
+#     i += 1
+
+# # Print 10 to -10
+# j = 10
+# while j>=-10:
+#     print(j)
+#     j -= 1
